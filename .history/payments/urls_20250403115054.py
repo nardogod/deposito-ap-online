afd5a75db65b4cc828a661e455c3c75f15aa82e7
Nardo Gomes <nardogomes.lg@gmail.com>
2025-04-04 11:59:17 -0300

    Remove .history do controle de versão e atualiza .gitignore

--- .history/payments/urls_20250403115054.py
+++
@@ -1,9 +0,0 @@
-# payments/urls.py
-from django.urls import path
-from .views import CreatePaymentPreferenceView, PaymentWebhookView, OrderByPreferenceView
-
-urlpatterns = [
-    path('create/', CreatePaymentPreferenceView.as_view(), name='create_payment_preference'),
-    path('webhook/', PaymentWebhookView.as_view(), name='payment_webhook'),
-    path('orders/by-preference/<str:preference_id>/', OrderByPreferenceView.as_view(), name='order_by_preference'),
-]
